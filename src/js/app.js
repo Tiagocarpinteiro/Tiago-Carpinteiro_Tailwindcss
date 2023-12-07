f1e3a40448916ab5cc828924d0b84e48b13026ce
Tiagocarpinteiro <tiago.carpinteiro07@gmail.com>
2023-12-07 15:11:49 +0100

    modification du formulaire

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,5 @@
 import Alpine from "alpinejs";
 import EditorPreview from "./components/EditorPreview";
-import FlightTable from "./components/FlightTable";
-import Slider from "./components/Slider";
 import Menu from "./components/Menu";
 
 window.Alpine = Alpine;
@@ -15,8 +13,6 @@ window.addEventListener('DOMContentLoaded', () => {
     window.alpineInitialized = true;
     const {data, start} = Alpine;
     data('EditorPreview', EditorPreview);
-    data('FlightTable', FlightTable);
-    data('Slider', Slider);
     data('Menu', Menu);
     start();
 });
